29cd486348b65a5b0d91d642c7dd4e3e4e6d3afa
Thomas Ludwig <business@tludwig.dev>
2024-06-26 21:29:11 +0200

    feat: fully implement boost

--- backend/src/clients/twitch/events/channel_points/BoostChannelPoint.ts
+++ backend/src/clients/twitch/events/channel_points/BoostChannelPoint.ts
@@ -2,6 +2,7 @@ import BaseChannelPoint from "./BaseChannelPoint";
 import {EventSubChannelRedemptionAddEvent} from "@twurple/eventsub-base";
 import {getConfig} from "../../../../helper/ConfigHelper";
 import getWebsocketServer from "../../../../App";
+import {sleep} from "../../../../../../helper/GeneralHelper";
 
 export default class BoostChannelPoint extends BaseChannelPoint {
     title = 'Boost'
@@ -11,11 +12,20 @@ export default class BoostChannelPoint extends BaseChannelPoint {
 
         const websocketServer = getWebsocketServer()
 
-        websocketServer.send('trigger_keyboard', {'name': 'ship', 'keys': ['A']})
+        //websocketServer.send('trigger_keyboard', {'name': 'ship', 'keys': ['a']})
 
         const shipApiData = await (await fetch(shipDiagnosticsConfig.url)).json()
 
-        // todo: i need a keyboard esp thingi to continue
+        if(shipApiData.gear_down) websocketServer.send('trigger_keyboard', {'name': 'ship', 'keys': ['l']})
+        if(shipApiData.scoop_deployed) websocketServer.send('trigger_keyboard', {'name': 'ship', 'keys': ['home']})
 
+        await sleep(100)
+
+        websocketServer.send('trigger_keyboard', {'name': 'ship', 'keys': ['tab']})
+
+        await sleep(100)
+
+        if(shipApiData.gear_down) websocketServer.send('trigger_keyboard', {'name': 'ship', 'keys': ['l']})
+        if(shipApiData.scoop_deployed) websocketServer.send('trigger_keyboard', {'name': 'ship', 'keys': ['home']})
     }
 }
