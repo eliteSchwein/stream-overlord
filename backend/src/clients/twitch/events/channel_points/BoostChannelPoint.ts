5610d8a9fe75cbef437c7c797a56de79acfd70e8
Thomas Ludwig <business@tludwig.dev>
2024-06-26 20:39:25 +0200

    feat: add temporary keyboard integration

--- backend/src/clients/twitch/events/channel_points/BoostChannelPoint.ts
+++ backend/src/clients/twitch/events/channel_points/BoostChannelPoint.ts
@@ -1,13 +1,17 @@
 import BaseChannelPoint from "./BaseChannelPoint";
 import {EventSubChannelRedemptionAddEvent} from "@twurple/eventsub-base";
 import {getConfig} from "../../../../helper/ConfigHelper";
+import getWebsocketServer from "../../../../App";
 
 export default class BoostChannelPoint extends BaseChannelPoint {
     title = 'Boost'
 
     async handle(event: EventSubChannelRedemptionAddEvent) {
         const shipDiagnosticsConfig = getConfig(/api ship_diagnostics/g)[0]
-        const keyboardConfig = getConfig(/api keyboard/g)[0]
+
+        const websocketServer = getWebsocketServer()
+
+        websocketServer.send('trigger_keyboard', {'name': 'ship', 'keys': ['A']})
 
         const shipApiData = await (await fetch(shipDiagnosticsConfig.url)).json()
 
