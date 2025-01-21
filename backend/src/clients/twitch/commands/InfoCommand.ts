6ecbfe48230fcc720923818c7dc7bfd5f117a7ee
tludwig <t.ludwig@schommer-media.de>
2025-01-21 18:17:45 +0100

    chore: rebrand project

--- backend/src/clients/twitch/commands/InfoCommand.ts
+++ backend/src/clients/twitch/commands/InfoCommand.ts
@@ -5,6 +5,6 @@ export default class InfoCommand extends BaseCommand{
     command = 'info'
 
     async handle(params: any, context: BotCommandContext) {
-        await context.reply("Ich nutze ein selbst geschriebenen Bot welcher open source ist: https://github.com/eliteSchwein/dynamic-scene/")
+        await context.reply("Ich nutze ein selbst geschriebenen Bot welcher open source ist: https://github.com/eliteSchwein/stream-controller/")
     }
 }
