195cb4e7ab7c5996a39a478153d3dc4573bcc160
tludwig <t.ludwig@schommer-media.de>
2025-05-19 18:13:34 +0200

    feat: add initial player structure

---
+++ backend/src/helper/PlayerHelper.ts
@@ -0,0 +1,15 @@
+const playerData = {
+    currentPlaylist: [],
+    currentSoundrequest: {
+        enabled: false,
+        newIndex: 0,
+        currentIndex: 0,
+        currentUser: null
+    },
+    currentSong: {
+        index: 0,
+        name: "",
+        position: 0,
+        end: 0
+    }
+}
