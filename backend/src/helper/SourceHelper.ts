c2964db785c93756fb980a88af283828e7730a53
tludwig <business@tludwig.dev>
2025-06-27 22:19:54 +0200

    debug shit

--- backend/src/helper/SourceHelper.ts
+++ backend/src/helper/SourceHelper.ts
@@ -36,6 +36,7 @@ export async function updateSourceFilters() {
             if(filterName.startsWith("Source|")) {
                 switch (filterName) {
                     case "Source|Transform":
+                        console.log(sourceItemData)
                         console.log(config)
                         await getOBSClient().getOBSWebSocket().call('SetSceneItemTransform', {
                             sceneUuid: sourceItemData.scene.uuid,
