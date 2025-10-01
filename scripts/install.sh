ae019036edd0f1e81b0518f6523bc0de491f2d7d
tludwig <business@tludwig.dev>
2025-10-01 21:51:32 +0200

    fix: user service instead of desktop autostart

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 mkdir -p ~/.config/systemd/user/
-mkdir -p ~/.config/autostart/
 
 cp stream-overlord.service ~/.config/systemd/user/
-cp firefox-commander.desktop ~/.config/autostart/
+cp firefox-commander.service ~/.config/systemd/user/
 
 sudo apt install -y evtest alsa-utils libespeak-ng1 espeak libespeak-dev
 
