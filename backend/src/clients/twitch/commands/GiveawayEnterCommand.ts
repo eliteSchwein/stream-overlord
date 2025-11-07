6a7b1e2af61a5416969b9ebf8919fa94038a8400
tludwig <business@tludwig.dev>
2025-11-07 10:10:28 +0100

    feat: for now disable follower check on giveaway enter command

--- backend/src/clients/twitch/commands/GiveawayEnterCommand.ts
+++ backend/src/clients/twitch/commands/GiveawayEnterCommand.ts
@@ -18,27 +18,27 @@ export default class GiveawayEnterCommand extends BaseCommand {
             return
         }
 
-        const broadcasterId = getPrimaryChannel().id
-        const targetUserId = user.id
+        //const broadcasterId = getPrimaryChannel().id
+        //const targetUserId = user.id
 
-        const followerData = await this.bot.api.channels.getChannelFollowers(broadcasterId, targetUserId)
+        //const followerData = await this.bot.api.channels.getChannelFollowers(broadcasterId, targetUserId)
 
-        const followDate = followerData.data[0]?.followDate
+        //const followDate = followerData.data[0]?.followDate
 
-        if(!followerData.data[0]) {
-            await this.replyCommandError(context, "Du musst mindestens 7 Tage folgen um teilnehmen zu können!")
-            return
-        }
+        //if(!followerData.data[0]) {
+        //    await this.replyCommandError(context, "Du musst mindestens 7 Tage folgen um teilnehmen zu können!")
+        //  return
+        //}
 
-        const daysSinceFollow =
-            followDate
-                ? Math.max(0, Math.floor((Date.now() - followDate.getTime()) / 86_400_000))
-                : 0;
+        //const daysSinceFollow =
+        //        followDate
+        //        ? Math.max(0, Math.floor((Date.now() - followDate.getTime()) / 86_400_000))
+        //        : 0;
 
-        if(daysSinceFollow < 7) {
-            await this.replyCommandError(context, "Du musst mindestens 7 Tage folgen um teilnehmen zu können!")
-            return
-        }
+        //if(daysSinceFollow < 7) {
+        //    await this.replyCommandError(context, "Du musst mindestens 7 Tage folgen um teilnehmen zu können!")
+        //    return
+        //}
 
         addGiveawayUser(user)
 
