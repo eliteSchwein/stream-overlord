c0639f6db2446da1013d143ebe8934e8d0c702c7
tludwig <business@tludwig.dev>
2026-02-16 18:55:35 +0100

    feat: add build essential requirement

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,7 +9,7 @@ cp firefox-commander.desktop ~/.config/autostart/
 systemctl --user daemon-reload
 systemctl --user enable stream-overlord
 
-sudo apt install -y evtest alsa-utils libespeak-ng1 espeak libespeak-dev curl liblgpio-dev
+sudo apt install -y evtest alsa-utils libespeak-ng1 espeak libespeak-dev curl liblgpio-dev build-essential
 
 bash install_polkit.sh
 
