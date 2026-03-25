30768bc5b1ec84d1afd5a1ed3f5ed59cd29f5d42
tludwig <business@tludwig.dev>
2026-03-25 20:28:43 +0100

    chore: improve touch build with better linkers

--- scripts/install.sh
+++ scripts/install.sh
@@ -19,4 +19,6 @@ bash install_polkit.sh
 
 bash migrateNode.sh
 
+sudo apt remove stream-overlord-touch
+
 sudo apt install ../touch/src-tauri/target/aarch64-unknown-linux-gnu/release/bundle/deb/stream-overlord-touch_0.1.0_arm64.deb
