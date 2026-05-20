3312ff09c6b14b3674910b252dec3368518d04f0
tludwig <business@tludwig.dev>
2026-05-20 17:26:12 +0200

    feat: add initial support for dotlottie

--- frontend/src/js/controller/AnimationController.ts
+++ frontend/src/js/controller/AnimationController.ts
@@ -1,18 +1,30 @@
 import BaseController from "./BaseController";
+import {Websocket} from "websocket-ts";
 import lottie, {AnimationItem} from "lottie-web";
+import "@dotlottie/player-component";
 
 export default class AnimationController extends BaseController {
     private target = this.element.dataset.target ?? "default"
     private src = this.element.dataset.src
     private animation: AnimationItem | null = null
+    private dotlottiePlayer: any = null
 
     websocketEndpoints = [
-        'notify_animation_update',
+        "notify_animation_update",
     ]
 
     async postConnect() {
         if (!this.src) return
 
+        if (this.src.endsWith(".lottie")) {
+            this.loadDotLottie()
+            return
+        }
+
+        this.loadLottieJson()
+    }
+
+    private loadLottieJson() {
         this.animation = lottie.loadAnimation({
             container: this.element,
             renderer: "svg",
@@ -22,21 +34,69 @@ export default class AnimationController extends BaseController {
         })
     }
 
-    async handleMessage(websocket: any, method: string, data: any) {
-        if (method !== 'notify_animation_update') return
-        if (!this.animation) return
+    private loadDotLottie() {
+        this.element.innerHTML = ""
+
+        const player = document.createElement("dotlottie-player") as any
+
+        player.setAttribute("src", this.src)
+        player.setAttribute("renderer", "svg")
+        player.setAttribute("autoplay", "false")
+        player.setAttribute("loop", "false")
+        player.style.width = "100%"
+        player.style.height = "100%"
+
+        this.element.appendChild(player)
+
+        this.dotlottiePlayer = player
+    }
+
+    async handleMessage(websocket: Websocket, method: string, data: any) {
+        if (method !== "notify_animation_update") return
         if (data.target && data.target !== this.target) return
+
+        if (this.animation) {
+            this.playLottieJson(data)
+            return
+        }
+
+        if (this.dotlottiePlayer) {
+            await this.playDotLottie(data)
+        }
+    }
+
+    private playLottieJson(data: any) {
+        if (!this.animation) return
 
         const startFrame = data.startFrame ?? data.start_frame ?? 0
         const stopFrame = data.stopFrame ?? data.stop_frame ?? this.animation.totalFrames
         const speed = data.speed ?? 1
+        const loop = data.loop ?? false
 
         this.applyVariables(data.variables ?? {})
 
-        this.animation.loop = data.loop ?? false
+        this.animation.loop = loop
         this.animation.setSpeed(speed)
         this.animation.goToAndStop(startFrame, true)
         this.animation.playSegments([startFrame, stopFrame], true)
+    }
+
+    private async playDotLottie(data: any) {
+        if (!this.dotlottiePlayer) return
+
+        const startFrame = data.startFrame ?? data.start_frame ?? 0
+        const speed = data.speed ?? 1
+
+        this.dotlottiePlayer.loop = data.loop ?? false
+        this.dotlottiePlayer.speed = speed
+
+        if (typeof this.dotlottiePlayer.seek === "function") {
+            this.dotlottiePlayer.seek(startFrame)
+        }
+
+        if (typeof this.dotlottiePlayer.play === "function") {
+            this.dotlottiePlayer.play()
+        }
     }
 
     private applyVariables(variables: any) {
