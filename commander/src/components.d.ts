2278649dd125d777530221381249dc91bdd275b4
tludwig <business@tludwig.dev>
2026-06-02 19:19:33 +0200

    chore: update packages

--- commander/src/components.d.ts
+++ commander/src/components.d.ts
@@ -27,7 +27,6 @@ declare module 'vue' {
     PowerDialog: typeof import('./components/dialogs/PowerDialog.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
-    SpeedtestCard: typeof import('./components/cards/SpeedtestCard.vue')['default']
     YoloboxAudio: typeof import('./components/yolobox/YoloboxAudio.vue')['default']
     YoloboxPreview: typeof import('./components/yolobox/YoloboxPreview.vue')['default']
     YoloboxSettings: typeof import('./components/yolobox/YoloboxSettings.vue')['default']
